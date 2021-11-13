2e8ef4d8773106dd407ba9ff1758c3d134d26d14
lucascsmenezes <77303240+lucascsmenezes@users.noreply.github.com>
2021-11-13 19:54:42 -0300

    atualização icones links

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,10 @@
+(function(){
 let insta = document.getElementById('link-insta')
 let face = document.getElementById('link-face')
 let tube = document.getElementById('link-tube')
 let spoti = document.getElementById('link-spoti')
+let dee = document.getElementById('link-dee')
+let whats = document.getElementById('link-what')
 let hover = document.getElementById('footer-links')
 
 hover.addEventListener('mouseleave', ()=>{
@@ -9,6 +12,8 @@ hover.addEventListener('mouseleave', ()=>{
     face.style.backgroundColor = "white"
     tube.style.backgroundColor = "white"
     spoti.style.backgroundColor = "white"
+    dee.style.backgroundColor = "white"
+    whats.style.backgroundColor = "white"
 })
 insta.addEventListener('mouseenter', ()=>{
     if(insta){
@@ -17,6 +22,8 @@ insta.addEventListener('mouseenter', ()=>{
         face.style.backgroundColor = "gray"
         tube.style.backgroundColor = "gray"
         spoti.style.backgroundColor = "gray"
+        dee.style.backgroundColor = "gray"
+        whats.style.backgroundColor = "gray"
     }
 })
 face.addEventListener('mouseenter', ()=>{  
@@ -26,6 +33,8 @@ face.addEventListener('mouseenter', ()=>{
         face.style.transition = "0.5s"
         tube.style.backgroundColor = "gray"
         spoti.style.backgroundColor = "gray"
+        dee.style.backgroundColor = "gray"
+        whats.style.backgroundColor = "gray"
     }
 })
 tube.addEventListener('mouseenter', ()=>{  
@@ -35,6 +44,8 @@ tube.addEventListener('mouseenter', ()=>{
         tube.style.backgroundColor = "white"
         tube.style.transition = "0.5s"
         spoti.style.backgroundColor = "gray"
+        dee.style.backgroundColor = "gray"
+        whats.style.backgroundColor = "gray"
     }
 })
 spoti.addEventListener('mouseenter', ()=>{  
@@ -44,8 +55,34 @@ spoti.addEventListener('mouseenter', ()=>{
         tube.style.backgroundColor = "gray"
         spoti.style.backgroundColor = "white"
         spoti.style.transition = "0.5s"
+        dee.style.backgroundColor = "gray"
+        whats.style.backgroundColor = "gray"
     }
 })
+dee.addEventListener('mouseenter', ()=>{
+    if(dee){
+        insta.style.backgroundColor = "gray"
+        face.style.backgroundColor = "gray"
+        tube.style.backgroundColor = "gray"
+        whats.style.backgroundColor = "gray"
+        spoti.style.backgroundColor = "gray"
+        dee.style.backgroundColor = "white"
+        dee.style.transition = "0.5s"
+    }
+})
+whats.addEventListener('mouseenter', ()=>{
+    if(whats){
+        insta.style.backgroundColor = "gray"
+        face.style.backgroundColor = "gray"
+        tube.style.backgroundColor = "gray"
+        whats.style.backgroundColor = "gray"
+        spoti.style.backgroundColor = "gray"
+        dee.style.backgroundColor = "gray"
+        whats.style.backgroundColor = "white"
+        whats.style.transition = "0.5s"
+    }
+})
+}());
 
 const btn = document.getElementById('checar')
 function toogleMenu(event) {
